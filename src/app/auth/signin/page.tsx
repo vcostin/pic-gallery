cf4e60524ff23d9950bca3a4a5123b2707ea1af5
Vadim Costin <vadim.costin@gmail.com>
2025-04-25 17:13:21 +0300

    Fix code errors

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -59,3 +59,4 @@ export default function SignIn() {
       </div>
     </div>
   );
+}
--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,5 +1,4 @@
-import { generateComponents } from "@uploadthing/react";
-import type { OurFileRouter } from "@/app/api/uploadthing/core";
+import { generateReactHelpers } from "@uploadthing/react";
+import type { ourFileRouter } from "@/app/api/uploadthing/core";
 
-export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<OurFileRouter>();
+export const { useUploadThing, uploadFiles } = generateReactHelpers<typeof ourFileRouter>();
